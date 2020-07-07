08cd7cc331e39c56d5d5aa685da6f42798dbb341
Amanda Henson <amkhenson@gmail.com>
2020-07-06 19:08:31 -0500

    Vehicle Questionaire

--- rubycars.rb
+++ rubycars.rb
@@ -1,2 +1,14 @@
-puts "Welcome to Ruby!"
-puts "Ruby takes practice"
+puts "What is make"
+make = gets
+puts "What is model"
+model = gets
+puts "What is year"
+year = gets
+puts "What is mileage"
+mileage = gets
+puts "What is color"
+color = gets
+puts ""
+puts "#{make} #{model} #{year} #{mileage} #{color}"
+
+
