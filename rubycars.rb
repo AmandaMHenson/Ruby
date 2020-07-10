af092d6272098b764e6a28e7f0cf501c9502ca38
Amanda Henson <amkhenson@gmail.com>
2020-07-09 19:14:51 -0500

    rubycarsmethod1

--- rubycars.rb
+++ rubycars.rb
@@ -1,14 +1,22 @@
-puts "What is make?"
-make = gets
-puts "What is model?"
-model = gets
-puts "What is year?"
-year = gets
-puts "What is mileage?"
-mileage = gets
-puts "What is color?"
-color = gets
-puts "What is price?"
-price = gets
+def car_question(description)
+    puts "What is #{description}?"
+    input = gets.chomp
+    input
+end
+
+def format_price(amount)
+   "$#{amount}" 
+end 
+
+def add_commas(num_string)
+    num_string.reverse.scan(/\d{3}|.+/).join(",").reverse
+  end
+  
+make = car_question("make")
+model = car_question("model")
+year = car_question("year")
+mileage = add_commas(car_question("mileage"))
+amount = car_question("amount")
+
 puts ""
-puts "#{make}#{model}#{year}#{mileage}#{color}$#{price}"
+puts " #{make.capitalize}\n #{model.capitalize}\n #{year} \n #{mileage}\n #{format_price(amount)}"
