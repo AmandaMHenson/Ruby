bdb02fa65044de2c18a62f63bc8b70f26e85b3e8
Amanda Henson <amkhenson@gmail.com>
2020-07-14 10:50:20 -0500

    create Vehicle and Vehicle_form classes

--- rubycars.rb
+++ rubycars.rb
@@ -1,23 +1,44 @@
-def car_question(description)
+class Vehicle 
+ 
+  attr_reader :make, :model, :year, :color
+  attr_writer :mileage, :asking_price
+
+  def initialize (make, model, year, color, mileage, asking_price)
+    @make = make.capitalize
+    @model = model.capitalize
+    @year = year
+    @color = color
+    @mileage = mileage
+    @asking_price = asking_price
+  end
+
+  def to_s
+    "Vehicle is a #{@year} #{@make} #{@model} (#{@color}) with #{@mileage} miles valued at $#{@asking_price}."
+  end
+end
+
+class Vehicle_form
+
+  def description
+   @make = car_detail("make")
+   @model = car_detail("model")
+   @year = car_detail("year")
+   @color = car_detail("color")
+   @mileage = car_detail("mileage")
+   @asking_price = car_detail("asking_price")
+  create_vehicle
+  end
+
+  def car_detail(description)
     puts "What is #{description}?"
     input = gets.chomp
     input
+  end
+
+ def create_vehicle
+  Vehicle.new(@make,@model,@year,@color,@mileage,@asking_price)
+ end
 end
 
-def format_price(asking_price)
-   "$#{asking_price}" 
-end 
-
-def add_commas(num_string)
-    num_string.reverse.scan(/\d{3}|.+/).join(",").reverse
-  end
-  
-make = car_question("make")
-model = car_question("model")
-year = car_question("year")
-color = car_question("color")
-mileage = add_commas(car_question("mileage"))
-asking_price = car_question("asking_price")
-
-puts ""
-puts " #{make.capitalize}\n #{model.capitalize}\n #{year} \n #{color.upcase} \n #{mileage}\n #{format_price(asking_price)}"
+vehicle1=Vehicle_form.new.description
+puts vehicle1
